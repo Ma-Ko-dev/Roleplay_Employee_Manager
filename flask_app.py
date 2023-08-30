8c5c97208a2b812298093b56ee58a145277181dc
Ma-Ko-dev <Mathias.Kowitz@gmx.de>
2023-08-30 15:32:04 +0200

    Initial upload
    Signed-off-by: Ma-Ko-dev <Mathias.Kowitz@gmx.de>

---
+++ flask_app.py
@@ -0,0 +1,125 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_login import login_required, LoginManager, login_user, logout_user
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from main import Employee, RegisteredUser
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, EqualTo, DataRequired
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MySecretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employee.db'
+
+# Login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.unauthorized_view = 'unauthorized'
+
+# SQLAlchemy Konfiguration
+engine = create_engine('sqlite:///employee.db')
+Session = sessionmaker(bind=engine)
+db_session = Session()
+
+
+# Register Form
+class RegistrationForm(FlaskForm):
+    username = StringField('Benutzername', validators=[InputRequired()])
+    password = PasswordField('Passwort', validators=[InputRequired()])
+    confirm_password = PasswordField('Passwort bestätigen', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Registrieren')
+
+
+# Login Form
+class LoginForm(FlaskForm):
+    username = StringField('Benutzername', validators=[DataRequired()])
+    password = PasswordField('Passwort', validators=[DataRequired()])
+    submit = SubmitField('Anmelden')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db_session.query(RegisteredUser).get(int(user_id))
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('Du musst eingeloggt sein, um auf diese Seite zuzugreifen.', 'danger')
+    return redirect(url_for('login'))
+
+
+@app.route('/')
+@login_required
+def index():
+    departments = db_session.query(Employee.department).distinct().all()
+    departments = [department[0] for department in departments]
+
+    if not departments:
+        departments.append("Keine Einträge gefunden.")
+
+    return render_template('index.html', departments=departments, title="Personal Verwaltungs System")
+
+
+@app.route('/department/<string:dep>')
+@login_required
+def department_list(dep):
+    employees = db_session.query(Employee).filter_by(department=dep).all()
+    return render_template('department_list.html', employees=employees, title="Mitarbeiterliste")
+
+
+@app.route('/employee/<int:employee_id>')
+@login_required
+def employee_detail(employee_id):
+    employee = db_session.query(Employee).get(employee_id)
+    return render_template('employee_detail.html', employee=employee)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        username = form.username.data.lower()
+        password = form.password.data
+
+        hashed_password = generate_password_hash(password)
+
+        new_user = RegisteredUser(username=username, password=hashed_password)
+        db_session.add(new_user)
+        db_session.commit()
+
+        db_session.close()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form, title="Registrierung")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data.lower()
+        password = form.password.data
+
+        user = db_session.query(RegisteredUser).filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            # Benutzer erfolgreich eingeloggt
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Falscher Benutzername oder Passwort', 'danger')
+
+    return render_template('login.html', form=form, title="Anmelden")
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey, Date, DateTime, Float
+from sqlalchemy.orm import declarative_base, relationship
+from flask_login import UserMixin
+
+# Datenbank initialisieren
+engine = create_engine('sqlite:///employee.db')
+Base = declarative_base()
+
+
+class Employee(Base):
+    __tablename__ = 'employees'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    position = Column(String)
+    ooc_age = Column(Integer)
+    ig_age = Column(Integer)
+    department = Column(String)
+    hire_date = Column(Date)
+    termination_date = Column(Date)
+    discord_handle = Column(String, nullable=False, unique=True)
+
+    # Hier die Beziehung zu den Notizen
+    notes = relationship("Note", back_populates="employee")
+    # Hier die Beziehung zu der Arbeitszeit
+    monthly_durations = relationship("MonthlyDuration", back_populates="employee")
+
+
+class Note(Base):
+    __tablename__ = 'notes'
+    id = Column(Integer, primary_key=True)
+    employee_id = Column(Integer, ForeignKey('employees.id'), nullable=False)
+    creator_name = Column(String)
+    text = Column(Text)
+    created_at = Column(DateTime, default=datetime.now)  # Neues Feld für das Erstellungsdatum
+
+    employee = relationship("Employee", back_populates="notes")
+
+
+class MonthlyDuration(Base):
+    __tablename__ = 'monthly_durations'
+    id = Column(Integer, primary_key=True)
+    employee_id = Column(Integer, ForeignKey('employees.id'), nullable=False)
+    month = Column(String, nullable=False)
+    year = Column(Integer, nullable=False)
+    duration_seconds = Column(Float, nullable=False)
+    last_seen = Column(DateTime, default=None)
+
+    employee = relationship("Employee", back_populates="monthly_durations")
+
+
+class RegisteredUser(Base, UserMixin):
+    __tablename__ = 'registered_users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+# # Tabelle in der Datenbank erstellen
+Base.metadata.create_all(engine)
+
+# Mit der Datenbank interagieren
+# Session = sessionmaker(bind=engine)
+# session = Session()
+
+# Mitarbeiter hinzufügen
+# new_employee = Employee(
+#     name='Gariot Grau',
+#     position='Leitender Branddirektor',
+#     ooc_age=38,
+#     ig_age=38,
+#     department='Feuerwehr',
+#     hire_date=datetime.strptime('15.06.2023', '%d.%m.%Y'),
+#     termination_date=None,
+#     discord_handle='Gariot.Grau#1234'
+# )
+
+# Füge den neuen Mitarbeiter zur Datenbank hinzu
+# session.add(new_employee)
+# session.commit()
+
+# Notiz hinzufügen
+# Finde den Mitarbeiter "John Doe" in der Datenbank
+# mitarbeiter = session.query(Employee).filter_by(name='Gariot Grau').first()
+
+# Notiz hinzufügen
+# new_note = Note(
+#     employee=mitarbeiter,
+#     creator_name='Alice',
+#     text='Eine wichtige Notiz für Gariot Grau!'
+# )
+
+# Füge die Notiz zur Datenbank hinzu
+# session.add(new_note)
+# session.commit()
+
+# Session schließen
+# session.close()
